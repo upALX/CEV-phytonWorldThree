fd13ee677b5c002c7b86868bef0973ca913f1ad3
José Alexandre <jose.antunes@qitech.com.br>
2022-09-14 00:42:45 -0300

    exercise 74 done

---
+++ challenges/CL15-0074.py
@@ -0,0 +1,17 @@
+# Escreva um programa que sorteie 5 valores e coloque esses dentro de uma tuple. Depois, pegue o maior e o menor valor de dentro dessa tuple e mostre eles assim como a tuple com os valores sorteados.
+
+from random import randrange
+
+list_numbers = []
+tuple_numbers = ()
+
+
+for counter in range(0, 5):
+    generator_number = randrange(0, 9)
+    list_numbers.append(generator_number)
+
+tuple_numbers = list_numbers
+
+print(f'Os valores criados foram armazenados na tupla: {tuple_numbers}')
+print(f'O menor valor é igual a: {min(tuple_numbers)}')
+print(f'O maior valor é igual a: {max(tuple_numbers)}')
